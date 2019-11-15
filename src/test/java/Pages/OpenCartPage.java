b27888dcefedbe5dc35f6d0e8777d1bc55417c19
gburlac <56862163+gburlac@users.noreply.github.com>
2019-11-15 16:56:06 +0200

    OpenCart scenario 1 Done.

--- src/test/java/Pages/OpenCartPage.java
+++ src/test/java/Pages/OpenCartPage.java
@@ -19,11 +19,51 @@ public class OpenCartPage {
     private static WebElement myAccount;
     @FindBy(xpath = "//ul[contains(@class, 'dropdown-menu dropdown-menu-right')]/li[2]/a")
     private static WebElement myAccountLogin;
+    @FindBy(xpath = "//input[@id='input-email']")
+    private static WebElement enterLoginCredentials;
+    @FindBy(xpath = "//input[@id='input-password']")
+    private static WebElement enterLoginPswd;
+    @FindBy(xpath = "//input[contains(@class, 'btn btn-primary')]")
+    private static WebElement pressLoginButon;
+    @FindBy(xpath = "//ul[contains(@class, 'nav navbar-nav')]/li[4]/a")
+    private static WebElement pressOnCategory;
+    @FindBy(xpath = "//div[contains(@class, 'button-group')]/button[1]")
+    private static WebElement clickOnAddToCart;
+    @FindBy(xpath = "//div[contains(@class, 'alert alert-success alert-dismissible')]/a[2]")
+    private static WebElement clickOnGoToCart;
+
 
     //method
+    public static void clickOnGoToCart() {
+        clickOnGoToCart.click();
+    }
+
+    public static void clickOnAddToCart() {
+        clickOnAddToCart.click();
+    }
+
+    public static void pressOnCategory() {
+        pressOnCategory.click();
+    }
+
+    public static void pressLoginButon() {
+        pressLoginButon.click();
+    }
+
+    public static void myAccountEnterPswd(String value) {
+        enterLoginPswd.sendKeys(value);
+    }
+
+    public static void myAccountEnterCr(String value) {
+        enterLoginCredentials.sendKeys(value);
+
+    }
+
     public static void myAccountLogin() {
         myAccountLogin.click();
-    }public static void myAccountClick() {
+    }
+
+    public static void myAccountClick() {
         myAccount.click();
     }
 
--- src/test/java/Util/Driver.java
+++ src/test/java/Util/Driver.java
@@ -8,13 +8,13 @@ import java.util.concurrent.TimeUnit;
 public class Driver {
     private static WebDriver driver;
 
-    public  static  void startDriver(){
+    public static void startDriver() {
 
-    System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 
-    driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
