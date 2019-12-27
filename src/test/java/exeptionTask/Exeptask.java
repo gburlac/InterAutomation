b38d4df00f012d3da29e69eac791d3e6b07182b8
gburlac <56862163+gburlac@users.noreply.github.com>
2019-12-27 14:55:18 +0200

    Adding matrice exemple.

--- src/test/java/exeptionTask/Exeptask.java
+++ src/test/java/exeptionTask/Exeptask.java
@@ -78,19 +78,76 @@ public class Exeptask {
 
     }
 
+    int[][] array = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+            {10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+            {20, 21, 22, 23, 24, 25, 26, 27, 28, 29},
+            {30, 31, 32, 33, 34, 35, 36, 37, 38, 39},
+            {40, 41, 42, 43, 44, 45, 46, 47, 48, 49},
+            {50, 51, 52, 53, 54, 55, 56, 57, 58, 59},
+            {60, 61, 62, 63, 64, 65, 66, 67, 68, 69},
+            {70, 71, 72, 73, 74, 75, 76, 77, 78, 79},
+            {80, 81, 82, 83, 84, 85, 86, 87, 88, 89},
+            {90, 91, 92, 93, 94, 95, 96, 97, 98, 99}};
+
+    public static int arraySum(int[][] array) {
+        int total = 0;
+
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++)
+                total += array[row][col];
+        }
+
+        return total;
+    }
 
     public static void main(String[] args) throws CustomExept {
 //        divideZero(5, 0);
 //        noFoundClass();
 //        noFoundClass2();
-        try {
-            testAssertStrings();
-        } catch (Throwable e) {
-            e.printStackTrace();
+//        try {
+//            testAssertStrings();
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//        }
+//
+//       throw new CustomExept("I'm fuck up");
+
+        int[][] matrix = {{1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                /*{1,2,3,4}*/};
+        int sum = 0;
+        for (int n = 0; n < matrix.length; n++) {
+            for (int m = 0; m < matrix[n].length; m++) {
+                System.out.print(matrix[n][m] + " ");
+                if (n == m) {
+                    sum += matrix[n][m];
+                }
+            }
+            System.out.println();
         }
+        System.out.println("Sum of matrix with 2 for lops: " + sum);
 
-       throw new CustomExept("I'm fuck up");
+        int total = 0;
+        for (int i = 0; i < matrix.length; i++) {
 
+            total += matrix[i][i];
+        }
+        System.out.println("Sum of matrix with 1 for lops: " + sum);
+
+
+        int[][] array = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+                {20, 21, 22, 23, 24, 25, 26, 27, 28, 29},
+                {30, 31, 32, 33, 34, 35, 36, 37, 38, 39},
+                {40, 41, 42, 43, 44, 45, 46, 47, 48, 49},
+                {50, 51, 52, 53, 54, 55, 56, 57, 58, 59},
+                {60, 61, 62, 63, 64, 65, 66, 67, 68, 69},
+                {70, 71, 72, 73, 74, 75, 76, 77, 78, 79},
+                {80, 81, 82, 83, 84, 85, 86, 87, 88, 89},
+                {90, 91, 92, 93, 94, 95, 96, 97, 98, 99}};
+        System.out.println("Array total: " + arraySum(array));
 
     }
 
